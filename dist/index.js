40ece0031b585e65158d9a5672a5fa3ed798564f
Michael Poteat <me@mpote.at>
2019-08-15 11:31:57 -0700

    Update package dependencies

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Y = (a) => ((b) => a((...c) => b(b)(...c)))((b) => a((...c) => b(b)(...c)));
--- dist/index.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-it("fac(5) = 120", () => {
-    expect(index_1.Y((rec) => (x, y = 1) => x === 1 ? y : rec(x - 1, x * y))(5)).toBe(120);
-});
