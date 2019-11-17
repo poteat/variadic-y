8f4fb378dea8b7e42052571f65a7ea2a0ec1d1f0
Michael Poteat <me@mpote.at>
2019-11-16 22:23:26 -0800

    Finish typing the Y combinator

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,19 +2,22 @@ import { Y } from "./index";
 
 it("fac(5) = 120", async () => {
   expect(
-    await Y((f: (x: number, y: number) => number) => (x: number, y = 1) =>
-      x === 1 ? y : f(x - 1, x * y)
+    Y(
+      (f: (x: number, y: number) => number) => (
+        x: number,
+        y: number = 1 as any
+      ) => (x === 1 ? y : f(x - 1, x * y))
     )(5)
   ).toBe(120);
 });
 
-it("Async functions >:)", async () => {
+it("Async functions", async () => {
   expect(
-    await (await Y(
-      async (f: (x: number, y: number) => Promise<number>) => async (
+    await Y(
+      (f: (x: number, y: number) => Promise<number>) => async (
         x: number,
         y = 1
       ) => (x === 1 ? y : f(x - 1, x * y))
-    ))(5)
+    )(5)
   ).toBe(120);
 });
