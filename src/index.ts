c73cb1d9471fe5a9345991ffd6a564dc7f3d6c43
poteat <poteat@users.noreply.github.com>
2019-11-16 22:23:26 -0800

    Finish typing the Y combinator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export const Y = (a: any) =>
-  ((b: any) => a(async (...c: any[]) => (await b(b))(...c)))((b: any) =>
-    a(async (...c: any[]) => (await b(b))(...c))
-  );
+type GType = (...args: any[]) => any;
+type G2Type = (fcn: GType) => GType;
+
+export const Y = <T extends G2Type>(fcn: T) =>
+  ((b: any) => fcn((...c: any[]) => b(b)(...c)))((b: any) =>
+    fcn((...c: any[]) => b(b)(...c))
+  ) as ReturnType<T>;
